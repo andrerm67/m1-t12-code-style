9601aee362f7c8bf4388fd22fe5f0aa07875c21c
andrerm67 <146099693+andrerm67@users.noreply.github.com>
2023-10-27 22:58:48 +0300

    Поправлено после ревью

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -1,47 +1,43 @@
 import java.util.Scanner;
 
 public class DepositCalculator {
-    double сalculateComplexPercent(double amount, double yearRate, int period ) { //лишний пробел перед закрывающей скобкой
-        double pay = amount * Math.pow((1 + yearRate / 12), 12 * period); //лишняя пустая сторока после вычислений
-
-        return round(pay, 2);
+    double calculateComplexPercent(double amount, double yearRate, int period) { //лишний пробел перед закрывающей скобкой + 
+        double pay = amount * Math.pow((1 + yearRate / 12), 12 * period); //лишняя пустая сторока после вычислений + 
+        return roundDecimalPlace(pay, 2);
     }
 
     double сalculateSimplePercent(double amount, double yearRate, int period) {
-        return round(amount + amount * yearRate * period, 2);
+        return roundDecimalPlace(amount + amount * yearRate * period, 2);
     }
     //название метода лучше более детализировать (например roundDecimalPlace - округление десятичных знаков),
-    // с текущим названием метода может выйти путаница со стандартным математическим методом округления
-    double round (double value, int places) {
+    // с текущим названием метода может выйти путаница со стандартным математическим методом округления + 
+    double roundDecimalPlace (double value, int places) {
         /*название переменных должно быть в формате lowerCamelCase -
-        в ScaLe - намешаны прописные и строчные буквы, а должны быть все маленькие*/
-        double ScaLe = Math.pow(10, places); // здесь тоже лишняя пустая строка после вычисления
-
+        в ScaLe - намешаны прописные и строчные буквы, а должны быть все маленькие +*/ 
+        double scaLe = Math.pow(10, places); // здесь тоже лишняя пустая строка после вычисления +
         return Math.round(value*ScaLe) / ScaLe;
     }
 
     void resultDeposit() {
-        int period;
-        int action;
         Scanner scanner = new Scanner(System.in); //тут пустая строка после объявления переменных добавлена правильно
 
         System.out.println("Введите сумму вклада в рублях:");
         int amount = scanner.nextInt();
 
         System.out.println("Введите срок вклада в годах:");
-        period = scanner.nextInt();
+        int period = scanner.nextInt();
 
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
-        action = scanner.nextInt();
+        int action = scanner.nextInt();
 
         double outputVariable = 0;
 
-        /*сalculateSimplePercent и сalculateComplexPercent - первая буква "с" - русская, лучше исправить на латиницу*/
+        /*сalculateSimplePercent и сalculateComplexPercent - первая буква "с" - русская, лучше исправить на латиницу +*/
         if (action == 1){
-            /*знак "=" должен быть выделен пробелами с двух сторон */
-            outputVariable= сalculateSimplePercent(amount, 0.06, period);
+            /*знак "=" должен быть выделен пробелами с двух сторон +*/
+            outputVariable = calculateSimplePercent(amount, 0.06, period);
         } else if (action == 2) {
-            outputVariable = сalculateComplexPercent(amount, 0.06, period);
+            outputVariable = calculateComplexPercent(amount, 0.06, period);
         }
         System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + outputVariable);
     }
